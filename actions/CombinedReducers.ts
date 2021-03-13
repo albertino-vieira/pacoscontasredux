dfb44dd2a8a34df9f532a842f9f4e34fd049c5d2
Albertino Vieira <albertino.tino.1@gmail.com>
2021-03-13 14:49:59 +0000

    [MAJOR] - First commit

---
+++ actions/CombinedReducers.ts
@@ -0,0 +1,12 @@
+import { combineReducers } from "redux"
+
+/**
+ * Combines all the reducers into a single one.
+ * This is the reducer passed to the store upon its creation.
+ */
+const CombinedReducers = combineReducers(
+    {
+      
+    }
+);
+export default CombinedReducers;
---
+++ states/AppState.ts
@@ -0,0 +1,13 @@
+
+/**
+ * Application state interface
+ * for type definition.
+ */
+export interface AppState {
+
+}
+/**
+ * Application initial state.
+ */
+export const initialState: AppState = {
+};
